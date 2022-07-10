19cb25ec86cb551a3766b315b4701acc04e33528
Alice <yangalice8@gmail.com>
2022-07-10 09:34:05 +0200

    fix keys

--- src/backend/real_time_voting/settings.py
+++ src/backend/real_time_voting/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from email.policy import default
 from pathlib import Path
+from .base import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-r^=$a&dbs7jys4np_!45+pt8v&2nt%#^tbcwkz3e5-3roaeu-l"
+SECRET_KEY = env(
+    "DJANGO_SECRET_KEY",
+    default="django-insecure-r^=$a&dbs7jys4np_!45+pt8v&2nt%#^tbcwkz3e5-3roaeu-l",
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
